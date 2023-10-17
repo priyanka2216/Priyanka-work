35b6cdeaa7483d6c3ee6ba358aea22dd00d3e610
priyanka2216 <priyanka221999@gmail.com>
2023-10-17 15:12:31 +0530

    setting.py code changes

--- Employee/settings.py
+++ Employee/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1w_ri6qsxj@lmm=5y5b-sdhr&clhot9rxbyys_4r58khdz(wz%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ams.webinfinitesolutions.com','www.ams.webinfinitesolutions.com']
 
 
 # Application definition
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'app1'
 ]
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -76,10 +75,21 @@ WSGI_APPLICATION = 'Employee.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'python_ams_db',
+        'USER': 'pyhton_ams_user',
+        'PASSWORD': 'ams_user_db123',
+        'HOST': 'python-ams.webinfinitesolutions.com',
+        'PORT': '',
     }
 }
 
@@ -119,12 +129,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = '/static/'
+STATIC_ROOT = '/home/dh_985r5b/ams.webinfinitesolutions.com/public/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
