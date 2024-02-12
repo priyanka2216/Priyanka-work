bab5d82196e31c4b762a8a5bdd0c229c3b57119f
priyanka2216 <priyanka221999@gmail.com>
2024-02-12 16:27:55 +0530

    add requirements.txt

---
+++ passenger_wsgi.py
@@ -0,0 +1,9 @@
+import sys
+import os
+
+# Set the Python path for your application
+sys.path.append(os.getcwd())
+sys.path.append('/home/dh_985r5b/ams.webinfinitesolutions.com')
+
+# Set the environment variable to inform Django about the production environment
+os.environ['DJANGO_SETTINGS_MODULE'] = 'ams.webinfinitesolutions.com.settings'
